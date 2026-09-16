1c4ba6d261efe137eb1f86850af6fcf14147945e
akivamac <akivamac@k4r.org>
2026-09-16 19:45:08 -0400

    feat: thern.py loops continuously for overnight training

--- thern.py
+++ thern.py
@@ -17,28 +17,31 @@ experts = ["coding", "cot", "emotion", "fish", "greeting",
 MAX_PARALLEL = 2
 running = []  # list of (proc, expert)
 
-i = 0
-while i < len(experts) or running:
-    while len(running) < MAX_PARALLEL and i < len(experts):
-        expert = experts[i]
-        proc = launch(['python3', 'training/train_expert.py',
-                       '--name', expert, '--steps', '4000',
-                       '--resume', '--push', '100',
-                       '--log', '25', '--sample', '25'])
-        running.append((proc, expert))
-        print(f"Started {expert} (PID {proc.pid})")
-        i += 1
+while True:
+    i = 0
+    running = []
+    while i < len(experts) or running:
+        while len(running) < MAX_PARALLEL and i < len(experts):
+            expert = experts[i]
+            proc = launch(['python3', 'training/train_expert.py',
+                           '--name', expert, '--steps', '4000',
+                           '--resume', '--push', '500',
+                           '--log', '25', '--sample', '25'])
+            running.append((proc, expert))
+            print(f"Started {expert} (PID {proc.pid})")
+            i += 1
 
-    time.sleep(60)
-    still = []
-    for proc, expert in running:
-        ret = proc.poll()
-        if ret is None:
-            still.append((proc, expert))
-        elif ret == 0:
-            print(f"Finished {expert}")
-        else:
-            print(f"CRASHED {expert} (exit code {ret})")
-    running = still
+        time.sleep(60)
+        still = []
+        for proc, expert in running:
+            ret = proc.poll()
+            if ret is None:
+                still.append((proc, expert))
+            elif ret == 0:
+                print(f"Finished {expert}")
+            else:
+                print(f"CRASHED {expert} (exit code {ret})")
+        running = still
 
-print("All done.")   
+    print("All done. Restarting in 30s...")
+    time.sleep(30)
